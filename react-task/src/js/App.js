c5afc1a14710c43d7a8d3a01d1abde2122daac17
Dmitrii Ponomarev <kikkyow18@gmail.com>
2021-04-09 12:47:07 +0400

    basic structure and dependencies

---
+++ react-task/src/js/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../scss/app.scss'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+         <h1>Some text</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
