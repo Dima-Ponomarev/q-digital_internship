6056a78c3d961c81c8962c2d3a6b9518ca1f5edd
Dmitrii Ponomarev <kikkyow18@gmail.com>
2021-04-12 13:14:33 +0400

    add slider with fetched images

--- src/js/pages/Slider.js
+++ src/js/pages/Slider.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchImages } from '../redux/actions'
+import Button from '../components/Button'
 
 export class Slider extends Component {
-  componentWillMount() {
+  state = {
+    index: 0
+  }
+
+  componentDidMount() {
     fetch('https://imagesapi.osora.ru/')
       .then(res => res.json())
       .then(data => this.props.fetchImages(data))
   }
 
+  onNext = () => {
+    this.setState({
+      index: (this.state.index + 1) % this.props.images.length
+    })
+  }
+
+  onPrev = () => {
+    if (this.state.index >= 1) {
+      this.setState({
+        index: this.state.index - 1
+      })
+    } else {
+      this.setState({
+        index: this.props.images.length - 1
+      })
+    }
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <main className='slider'>
+        <div className='slider__wrapper'>
+          <Button text='prev' onChange={this.onPrev}/>
+          {this.props.images 
+          && (
+            <img className='slider__image' src={this.props.images[this.state.index]} alt='slider'/>
+          )}
+          <Button text='next' onChange={this.onNext}/>
+        </div>
+        <Button text='switch to local'/>
+        <Button text='back to main' type='link' to='/'/>
+      </main>
     )
   }
 }
