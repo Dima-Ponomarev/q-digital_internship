4e92f8b35d516f93af8fa65204539501a8cc37f9
Dmitrii Ponomarev <kikkyow18@gmail.com>
2021-04-12 12:39:33 +0400

    update button component

--- src/js/components/Button.js
+++ src/js/components/Button.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
 export class Button extends Component {
   render() {
+    if (this.props.type === 'link'){
+      return (
+        <Link className='button button_link' to={this.props.to}>
+          {this.props.text}
+        </Link>
+      )
+    }
     return (
-      <div className='button'>
+      <div className='button' onClick={this.props.onChange}>
         {this.props.text}
       </div>
     )
