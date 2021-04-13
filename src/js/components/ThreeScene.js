bc3438f592d3eb2533c25d749cc1c7e2916c1a97
Dmitrii Ponomarev <kikkyow18@gmail.com>
2021-04-13 11:54:03 +0400

    add rotating sphere

--- src/js/components/ThreeScene.js
+++ src/js/components/ThreeScene.js
@@ -1,48 +1,68 @@
 import * as THREE from 'three'
 
 export default class ThreeScene {
+  #renderer
+  #scene
+  #camera
+
   constructor(root){
     this.root = root
   }
 
-  drawPlane = (scene) => {
+  drawPlane = () => {
     const planeGeometry = new THREE.PlaneGeometry(4.5, 4.5)
     const material = new THREE.MeshBasicMaterial({ 
       color: 0x0000ff, 
       side: THREE.DoubleSide 
     })
     const plane = new THREE.Mesh( planeGeometry ,material )
-    plane.rotateX( Math.PI - 1 ) 
-    plane.rotateZ( Math.PI - 1 )
-    scene.add( plane )
+    plane.rotateX( Math.PI - 1.2 ) 
+    plane.rotateZ( Math.PI - 1.1 )
+    plane.position.set(0, -1, 0)
+    this.#scene.add( plane )
   }
 
-  drawSphere = (scene) => {
-    const sphereGeometry = new THREE.SphereGeometry( 1, 64, 64 )
-    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    const sphere = new THREE.Mesh( sphereGeometry, material );
-    scene.add( sphere );
+  drawSphere = () => {
+    const sphereGeometry = new THREE.SphereGeometry( 1, 45, 45 )
+    const edges = new THREE.EdgesGeometry( sphereGeometry )
+
+    const material = new THREE.LineBasicMaterial( {
+      color: 0x00ff00,
+      linewidth: 4,
+    } );
+
+    const line = new THREE.LineSegments( edges, material )
+
+    this.#scene.add( line );
+
+    const rotate = () => {
+      requestAnimationFrame( rotate )
+      line.rotation.y += 0.01
+      this.#renderer.render( this.#scene, this.#camera)
+    }
+
+    rotate()
   }
 
   init = () => {
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera( 
+    this.#scene = new THREE.Scene()
+    this.#camera = new THREE.PerspectiveCamera( 
       75, 
       window.innerWidth / window.innerHeight, 
       0.1, 
       1000 
     );
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    this.root.appendChild( renderer.domElement )
+    this.#renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.#renderer.setSize( window.innerWidth, window.innerHeight )
+    this.root.appendChild( this.#renderer.domElement )
 
-    camera.position.z = 5
+    this.#camera.position.z = 5
 
-    this.drawPlane(scene);
-    this.drawSphere(scene)
+    this.drawPlane(this.#scene);
+    this.drawSphere(this.#scene)
 
-    renderer.render( scene, camera )
+    this.#renderer.render( this.#scene, this.#camera )
   }
 
 
