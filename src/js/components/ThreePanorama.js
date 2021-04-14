83a5f6dbde0f2b251e599daa7bda5d7ee03a57b5
Dmitrii Ponomarev <kikkyow18@gmail.com>
2021-04-14 12:30:39 +0400

    add second sphere

--- src/js/components/ThreePanorama.js
+++ src/js/components/ThreePanorama.js
@@ -2,26 +2,35 @@ import * as THREE from 'three'
 import data from './data'
 
 export default class ThreePanorama{
-  renderer
-  scene
-  camera
+  #renderer
+  #scene
+  #camera
   #sizes = {
     width: window.innerWidth,
     height: window.innerHeight
   }
+  
 
   constructor(root){
     this.root = root
     this.init()
   }
 
-  drawSphere = () => {
+  drawSphere = (dataIndex) => {
     const geometry = new THREE.SphereGeometry(500, 60, 40)
     geometry.scale( - 1, 1, 1 )
-    const loader = new THREE.TextureLoader()
-    const material = new THREE.MeshBasicMaterial({
-       map: loader.load(data[0].path)
+
+    let material, loader
+    if (typeof dataIndex !== 'undefined'){
+      loader = new THREE.TextureLoader()
+      material = new THREE.MeshBasicMaterial({
+        map: loader.load(data[dataIndex].path)
     });
+    } else {
+      material = new THREE.MeshBasicMaterial({
+        opacity: 0
+      })
+    }
     return new THREE.Mesh( geometry, material )
     
   }
@@ -41,40 +50,44 @@ export default class ThreePanorama{
   }
 
   init = () => {
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(
+    this.#scene = new THREE.Scene()
+    this.#camera = new THREE.PerspectiveCamera(
       90, 
       this.#sizes.width / this.#sizes.height,
       0.1, 
       1000 
     )
-    this.camera.target = new THREE.Vector3( 0, 0, 0 )
+    this.#camera.position.z = 5
+    this.#camera.lookAt(0, 0)
 
-    this.renderer = new THREE.WebGLRenderer({ antialias: true })
-    this.renderer.setSize( this.#sizes.width, this.#sizes.height )
-    this.root.appendChild( this.renderer.domElement )
 
-    const sphereMesh = this.drawSphere()
-    this.scene.add(sphereMesh)
+    this.#renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.#renderer.setSize( this.#sizes.width, this.#sizes.height )
+    this.root.appendChild( this.#renderer.domElement )
+
+    const mainSphereMesh = this.drawSphere(0)
+
+    const otherSphereMesh = this.drawSphere()
+    otherSphereMesh.position.z = -10
+
     const arrowMesh = this.drawArrow()
 
-    //this.scene.add(arrowMesh)
-    sphereMesh.add(arrowMesh)
-
-    this.camera.position.z = 5
-    this.camera.lookAt(0, 0)
+    this.#scene.add(mainSphereMesh)
+    this.#scene.add(otherSphereMesh)
+    this.#scene.add(arrowMesh)
+    //mainSphereMesh.add(arrowMesh)
 
     const animate = () => {
       requestAnimationFrame( animate )
 
-      this.renderer.render( this.scene, this.camera )
+      this.#renderer.render( this.#scene, this.#camera )
     };
-    this.renderer.render( this.scene, this.camera )
+
+    this.#renderer.render( this.#scene, this.#camera )
 
     //Анимация нужна иначе меш с загружаемой текстурой 
     //не показывается (Есть способ лучше?)
     animate()
-
 
 
     //handle resize
@@ -82,10 +95,10 @@ export default class ThreePanorama{
       this.#sizes.width = window.innerWidth
       this.#sizes.height = window.innerHeight
 
-      this.camera.aspect = this.#sizes.width / this.#sizes.height
-      this.camera.updateProjectionMatrix()
+      this.#camera.aspect = this.#sizes.width / this.#sizes.height
+      this.#camera.updateProjectionMatrix()
 
-      this.renderer.setSize( this.#sizes.width, this.#sizes.height )
+      this.#renderer.setSize( this.#sizes.width, this.#sizes.height )
     })
   }
 }
