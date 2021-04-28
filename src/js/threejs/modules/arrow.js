8a932ea3bce6d75047d62350084dbbeb56f0d6ce
Dmitrii Ponomarev <kikkyow18@gmail.com>
2021-04-28 18:05:27 +0400

    3d arrow

--- src/js/threejs/modules/arrow.js
+++ src/js/threejs/modules/arrow.js
@@ -10,18 +10,26 @@ export default class Arrow extends Model{
   #createMesh = (x, y, z, id, direction) => {
     const radius = 4
 
-    const geometry = new THREE.ConeGeometry(0.7, 0.5, 3)
-    const material = new THREE.MeshBasicMaterial({ 
+    const coneGeometry = new THREE.ConeGeometry(0.7, 0.5, 3)
+    const coneMaterial = new THREE.MeshBasicMaterial({ 
       color: 0x750000
     })
-    const arrowMesh = new THREE.Mesh(geometry, material)
+    const arrowMesh = new THREE.Mesh(coneGeometry, coneMaterial)
+
+    const boxGeometry = new THREE.BoxGeometry(1.32, 0.2, 0.01)
+    const boxMaterial = new THREE.MeshBasicMaterial({ 
+      color: 0x5c0000
+    })
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
 
     const directionVec = new THREE.Vector3(x, y, z)
     const normalVec = new THREE.Vector3(0, 0, 1)
     
     const pivot = new THREE.Object3D()
     arrowMesh.position.set(0, -2, radius)
+    boxMesh.position.set(0, -2.56, radius)
     arrowMesh.userData = {type: 'arrow', id: id}
+    boxMesh.userData = {type: 'arrow', id: id}
     
     if (x >= 0){
       pivot.rotateY(normalVec.angleTo(directionVec) + THREE.Math.degToRad(direction))
@@ -29,7 +37,9 @@ export default class Arrow extends Model{
       pivot.rotateY(-normalVec.angleTo(directionVec) + THREE.Math.degToRad(direction))
     }
 
-    pivot.add(arrowMesh)
+    pivot.rotateX(0.1)
+
+    pivot.add(arrowMesh, boxMesh)
 
     return pivot
   }
